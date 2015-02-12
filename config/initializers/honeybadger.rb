c5743e0f14ba4bde49896bdb498194cd82e9cd5a
Joshua Wood <josh@hintmedia.com>
2015-02-12 11:22:34 -0800

    Update to honeybadger 2.0.

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,6 +1,1 @@
 class IgnoredError < StandardError; end
-Honeybadger.configure do |config|
-  config.api_key = ENV['HONEYBADGER_API_KEY']
-  config.development_environments = []
-  config.ignore.push 'IgnoredError'
-end
